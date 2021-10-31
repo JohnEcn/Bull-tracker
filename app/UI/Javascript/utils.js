a426ba3f7c22dfddc6b9cf6aa2d1cb2baddd08de
= <Giannhs.econ@hotmail.com>
2021-10-31 03:52:58 +0300

    Added loading animation

--- app/UI/Javascript/utils.js
+++ app/UI/Javascript/utils.js
@@ -1,4 +1,13 @@
 function blurBg(px)
 {
     document.getElementById("mainContainer").style.filter = "blur("+px+"px)";
+    if(px == 0)
+    {
+        document.getElementById("loadingGif").style.display =  "none";
+    }
+    else
+    {
+        document.getElementById("loadingGif").style.display =  "initial";
+    }
 }
+    
