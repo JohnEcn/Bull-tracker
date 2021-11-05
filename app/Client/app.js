ce9ea32232c51208e735443829954f7cf2a8d91e
= <Giannhs.econ@hotmail.com>
2021-11-05 19:55:37 +0200

    Added new feature that makes possible for the user to select currency and dataRange

--- app/Client/app.js
+++ app/Client/app.js
@@ -1,20 +1,27 @@
 {
     let coinList = [];
 
-    const dataDaysBefore =7;
-    const currency = "eur";
-    const currencySymbol = "€";
-    const interval = "hourly";  
+    let dataDaysBefore =1;
+    let currency = "eur";
+    let currencySymbol = "€";
+    let interval = "hourly";  
     
     let timeoutId;
-    
+    function initParameters()
+    {
+        dataDaysBefore = JSON.parse(localStorage.getItem("daysRange"));
+        currency = JSON.parse(localStorage.getItem("currency"))[0];
+        currencySymbol = JSON.parse(localStorage.getItem("currency"))[1];
+        interval =  dataDaysBefore > 7 ? "daily" : "hourly";
+    }
     function loadData()
     {
+        initParameters();
         //if this timeout does not get cancelled in time then a delay message appears
         timeoutId = setTimeout(function()
         {
             let msg  = "There are some delays using the Coingkeco API.";
-            displayErrorMsg(msg,5);
+            displayErrorMsg(msg);
 
         },7000);
 
@@ -32,6 +39,7 @@
     function launchApp(rawCoinData,requestErrors = [])
     {
         clearTimeout(timeoutId);
+        hideErrorMsg()
         if(requestErrors.length != 0)
         {
             let msg  = "There was an error retrieving the coin data. Please try again in 30 seconds.";
